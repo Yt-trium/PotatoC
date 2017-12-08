df22ff3a53d0f6476e96a0b8a72292df5e6964df
Yt-trium <cyril.meyer.68@gmail.com>
2017-12-08 20:16:09 +0100

    update var name

--- src/mips.c
+++ src/mips.c
@@ -3,15 +3,15 @@
 void toMips(symbol st, quad_list qt)
 {
     // out stream
-    // FILE* os = fopen("out.asm", "w");
-    FILE* os = stdout;
+    FILE* os = fopen("out.asm", "w");
+    // FILE* os = stdout;
 
     fprintf(os, ".data\n");
 
     while (st != NULL) {
         if(st->type == SYMBOL_CST)
         {
-            fprintf(os, "%s:\t.word %d\n", st->name, st->value);
+            fprintf(os, "PTC_%s_:\t.word %d\n", st->name, st->value);
         }
         st = st->next;
     }
@@ -25,39 +25,39 @@ void toMips(symbol st, quad_list qt)
         switch (qt->q->type)
         {
         case QUAD_OP_PLUS:
-            fprintf(os, "    lw $t0, %s\n", qt->q->left->name);
-            fprintf(os, "    lw $t1, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->left->name);
+            fprintf(os, "    lw $t1, PTC_%s_\n", qt->q->right->name);
             fprintf(os, "    add $t2, $t0, $t1\n");
-            fprintf(os, "    sw $t2, %s\n", qt->q->res->name);
+            fprintf(os, "    sw $t2, PTC_%s_\n", qt->q->res->name);
             break;
         case QUAD_OP_MINUS:
-            fprintf(os, "    lw $t0, %s\n", qt->q->left->name);
-            fprintf(os, "    lw $t1, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->left->name);
+            fprintf(os, "    lw $t1, PTC_%s_\n", qt->q->right->name);
             fprintf(os, "    sub $t2, $t0, $t1\n");
-            fprintf(os, "    sw $t2, %s\n", qt->q->res->name);
+            fprintf(os, "    sw $t2, PTC_%s_\n", qt->q->res->name);
             break;
         case QUAD_OP_MULT:
-            fprintf(os, "    lw $t0, %s\n", qt->q->left->name);
-            fprintf(os, "    lw $t1, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->left->name);
+            fprintf(os, "    lw $t1, PTC_%s_\n", qt->q->right->name);
             fprintf(os, "    mult $t2, $t0, $t1\n");
-            fprintf(os, "    sw $t2, %s\n", qt->q->res->name);
+            fprintf(os, "    sw $t2, PTC_%s_\n", qt->q->res->name);
             break;
         case QUAD_OP_DIVI:
-            fprintf(os, "    lw $t0, %s\n", qt->q->left->name);
-            fprintf(os, "    lw $t1, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->left->name);
+            fprintf(os, "    lw $t1, PTC_%s_\n", qt->q->right->name);
             fprintf(os, "    div $t2, $t0, $t1\n");
-            fprintf(os, "    sw $t2, %s\n", qt->q->res->name);
+            fprintf(os, "    sw $t2, PTC_%s_\n", qt->q->res->name);
             break;
         // UOP -> RIGHT
         case QUAD_UOP_MINUS:
-            fprintf(os, "    lw $t0, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->right->name);
             fprintf(os, "    sub $t0, $t0, 1\n");
-            fprintf(os, "    sw $t0, %s\n", qt->q->res->name);
+            fprintf(os, "    sw $t0, PTC_%s_\n", qt->q->res->name);
             break;
         case QUAD_UOP_PLUS:
-            fprintf(os, "    lw $t0, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->right->name);
             fprintf(os, "    add $t0, $t0, 1\n");
-            fprintf(os, "    sw $t0, %s\n", qt->q->res->name);
+            fprintf(os, "    sw $t0, PTC_%s_\n", qt->q->res->name);
             break;
         case QUAD_PRINTI:
             fprintf(os, "    li $v0, 1\n");
@@ -65,13 +65,13 @@ void toMips(symbol st, quad_list qt)
             break;
         // ASSIGN
         case QUAD_UOP_ASSIGN:
-            fprintf(os, "    lw $t0, %s\n", qt->q->right->name);
-            fprintf(os, "    sw $t0, %s\n", qt->q->res->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->right->name);
+            fprintf(os, "    sw $t0, PTC_%s_\n", qt->q->res->name);
             break;
         // GOTO
         case QUAD_GOTO_IF:
-            fprintf(os, "    lw $t0, %s\n", qt->q->left->name);
-            fprintf(os, "    lw $t1, %s\n", qt->q->right->name);
+            fprintf(os, "    lw $t0, PTC_%s_\n", qt->q->left->name);
+            fprintf(os, "    lw $t1, PTC_%s_\n", qt->q->right->name);
             switch (qt->q->cmp)
             {
             case QUAD_RELOP_EQ:
